487dd0f9e1fb892d42a5118ae70d473fb09b2309
nutanvithani <nutanvithani7@gmail.com>
2024-08-15 15:22:20 +0530

    first commit

---
+++ js/counter.js
@@ -0,0 +1,37 @@
+	$.fn.jQuerySimpleCounter = function( options ) {
+	    var settings = $.extend({
+	        start:  0,
+	        end:    100,
+	        easing: 'swing',
+	        duration: 400,
+	        complete: ''
+	    }, options );
+
+	    var thisElement = $(this);
+
+	    $({count: settings.start}).animate({count: settings.end}, {
+			duration: settings.duration,
+			easing: settings.easing,
+			step: function() {
+				var mathCount = Math.ceil(this.count);
+				thisElement.text(mathCount);
+			},
+			complete: settings.complete
+		});
+	};
+
+
+$('#number1').jQuerySimpleCounter({end: 21,duration: 2500});
+$('#number2').jQuerySimpleCounter({end: 17,duration: 2500});
+$('#number3').jQuerySimpleCounter({end: 24,duration: 2500});
+$('#number4').jQuerySimpleCounter({end: 38,duration: 2500});
+
+
+
+  	/* AUTHOR LINK */
+     $('.about-me-img').hover(function(){
+            $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+        }, function(){
+            $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+        });
+  
